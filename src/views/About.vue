fc712f8ae0bcddc4cf05db45d23ab1503505c983
jfkeci <ja.lip132@gmail.com>
2022-02-20 12:52:23 +0100

    chore: preview pdf

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,8 +1,10 @@
 <template>
-  <div>
+  <div style="height: 100vh">
+    <h1 class="mb-3">About</h1>
     <iframe
-      src="https://infallible-lalande-6f74b6.netlify.app/?viewer=pdf&file="
-      frameborder="0"
+      src="https://festive-hermann-9b9847.netlify.app/?viewer=google&file=https://github.com/jfkeci/picture-gallery/raw/main/src/assets/pdf/jakov-filip-sabolicek-cv.pdf"
+      height="80%"
+      width="80%"
     ></iframe>
   </div>
 </template>
