5927fabb6024a57928f8ba48326312386e2c2142
jfkeci <ja.lip132@gmail.com>
2022-02-20 12:53:09 +0100

    chore: preview pdf

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,7 @@
 <template>
   <div style="height: 100vh">
     <h1 class="mb-3">About</h1>
+    // My react-doc-preview app hosted on netlify
     <iframe
       src="https://festive-hermann-9b9847.netlify.app/?viewer=google&file=https://github.com/jfkeci/picture-gallery/raw/main/src/assets/pdf/jakov-filip-sabolicek-cv.pdf"
       height="80%"
